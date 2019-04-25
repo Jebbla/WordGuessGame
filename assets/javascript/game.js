1a4c43750a7dd8735dcd4e0b2a3fd4b2e5613f80
Jebbla <jlp2722@gmail.com>
2019-04-25 09:20:33 -0500

    initial

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+var selectableWords = [
+    "Maine Coon"
+    "Sphynx"
+    "Norwegian Forest"
+    "Persian"
+    "Russian Blue"
+    "Scottish Fold"
+    "Ragdoll"
+    "Siamese"
+    "Bengal"
+    "Burmese"
+    "Himalayan"
+    "Savannah"
+    "Manx"
+    "Siberian"
+    "British Shorthair"
+]
